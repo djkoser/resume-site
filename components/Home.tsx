63148665154c5beb49cb08124c573a7a55658e9a
David Koser <koser.david@gmail.com>
2022-11-08 10:28:18 -0600

    fix: update text content and resume

--- components/Home.tsx
+++ components/Home.tsx
@@ -20,11 +20,12 @@ export function Home() {
         <article className="textBlocks" id="homeDescription">
           Hello! My name is David Koser
           <br />I am an emerging software engineer and web developer focusing on
-          Typescript in a React.js environment. My resume can be viewed and
-          downloaded witin the &quot;Resume&quot; section. Screenshots,
-          descriptions and links to my previous projects and their associated
-          GitHub respositories can also be found within the &quot;Projects&quot;
-          section.
+          Typescript applied to Node.js and React.js. Currently, I am also actively
+          improving my mastery of AWS and associated cloud-based technologies.
+          My resume can be viewed and downloaded witin the &quot;Resume&quot;
+          section. Screenshots, descriptions and links to my previous projects
+          and their associated GitHub respositories can also be found within
+          the &quot;Projects&quot; section.
         </article>
       </div>
     </section>
